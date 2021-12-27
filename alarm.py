ce0e9bf3c58819b48c7f69783028d7ed4315301b
cheeseNA <kurocat2000@gmail.com>
2021-12-27 21:37:16 +0900

    Add sound playing function to alarm.py

--- alarm.py
+++ alarm.py
@@ -1,2 +1,15 @@
+import simpleaudio
+
+
 def alarm():
     print('alarm')
+    wave_obj = simpleaudio.WaveObject.from_wave_file(
+        '/usr/share/sounds/alsa/Front_Center.wav')
+    play_obj = wave_obj.play()
+    print('playing')
+    play_obj.wait_done()
+    print('playing done')
+
+
+if __name__ == '__main__':
+    alarm()
